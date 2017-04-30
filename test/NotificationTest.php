c00071ca5700108b3c51c4f98e41512a86f53bbe
Jimvirle <jimvirle@gmail.com>
2017-04-30 21:49:56 +0800

    Add .travis.yml, add phpunit on require-dev at composer.json and add a test case for Notification

---
+++ test/NotificationTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the JiNexus/Zend-Notification package.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed along with this source code.
+ *
+ *
+ * @link      https://github.com/JiNexus/zend-notification
+ * @copyright Copyright (c) 2017 Jimvirle Calago (JiNexus) <jinexus.zend@gmail.com>
+ * @license   https://github.com/JiNexus/zend-notification/license BSD 3-Clause License
+ */
+
+namespace JiNexus\Zend\NotificationTest;
+
+use JiNexus\Zend\Notification\Notification;
+use PHPUnit\Framework\TestCase;
+
+class NotificationTest extends TestCase
+{
+
+}
